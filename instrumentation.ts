23dd4d6940f31b721d64f1c7ebb921f921f47ce2
mariankugiel <marian.kugiel@saluso.com>
2025-12-06 11:59:35 +0100

    next.js update

---
+++ instrumentation.ts
@@ -0,0 +1,7 @@
+// Empty instrumentation file to satisfy Next.js requirements
+// This file is required by Next.js 15.5.7+ even if you don't use instrumentation
+
+export async function register() {
+  // Empty - no instrumentation needed
+}
+
